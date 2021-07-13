241143d2f810c87ecd75e1897a7a2e3e4ebcdc06
victorbohrer <de.bohrer@hotmail.com>
2021-07-12 23:54:24 -0300

    initial commit, first project part

---
+++ pages/index.js
@@ -0,0 +1,64 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/alurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
+
+function ProfileSidebar(props) {
+  
+  
+  return (
+    <Box>
+        <img src={`https:/github.com/${props.githubUser}.png`} style={{borderRadius: '8px' }} />
+    </Box>
+  )
+}
+
+
+export default function Home() {
+
+  const pessoasFavoritas = ['marcobrunodev', 'danielhe4rt', 'peas', 'juunegreiros']
+  
+  const githubUser = 'victorbohrer';
+  return (
+  <>
+    <AlurakutMenu/>
+    <MainGrid> 
+      <div className="profileArea" style={{gridArea: 'profileArea'}}> 
+        <ProfileSidebar githubUser={githubUser}/>
+      </div>
+
+      <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}> 
+        <Box>
+          <h1 className="title">
+            Bem-vindo(a)
+          </h1>
+
+          <OrkutNostalgicIconSet/>
+        </Box>
+      </div>
+
+      <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}> 
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smalltitle"> 
+            Pessoas da comunidade ({pessoasFavoritas.length})
+          </h2>
+
+          <ul>
+            {pessoasFavoritas.map((itemAtual) => {
+
+              return (
+                <li>
+                  <a href={`/users/${itemAtual}`} key={itemAtual}>
+                    <img src={`https://github.com/${itemAtual}.png`} />
+                    <span>{itemAtual}</span>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+      </div>      
+    </MainGrid>
+  </>
+  )
+}
---
+++ src/components/MainGrid/index.js
@@ -0,0 +1,28 @@
+
+import styled from 'styled-components'
+
+const MainGrid = styled.main`
+    width: 100%;
+    grid-gap: 10px;
+    padding: 16px;
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 500px;
+
+    .profileArea {
+        display: none;
+        @media(min-width: 860px) {
+            display: block
+        }
+    }
+  
+    @media(min-width: 860px){
+        max-width: 1110px;
+        grid-template-areas: 
+        "profileArea welcomeArea profileRelationsArea"; 
+        grid-template-columns: 160px 1fr 312px;
+        display: grid;
+    }
+`
+
+export default MainGrid;
