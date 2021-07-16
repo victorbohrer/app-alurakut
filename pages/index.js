a8dcecb29c9eb3b105d1bbb9fc018ec60f5e2dca
victorbohrer <de.bohrer@hotmail.com>
2021-07-16 20:53:34 -0300

    project finish

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/alurakutCommons'
@@ -44,8 +46,8 @@ function ProfileRelationsBox(props) {
 }
 
 
-export default function Home() {
-  const usuarioAleatorio = 'victorbohrer';
+export default function Home(props) {
+  const usuarioAleatorio = props.githubUser;
 
   const [comunidades, setComunidades] = React.useState([]);
   //   id: '23123231321',
@@ -219,4 +221,33 @@ export default function Home() {
     </MainGrid>
   </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN
+
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token) 
+  
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
